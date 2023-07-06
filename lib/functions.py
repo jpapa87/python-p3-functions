ff89bb746c82fb66130df15f44e4b9736d233e87
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-07-06 15:23:50 -0400

    submissions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Jon")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello,", name + "!")
+
 
 def add(num1, num2):
-    pass
+    return(num1 + num2 )
+    result= add(45 , 55)
+    print(result)
+
 
 def halve(number):
-    pass
+    return(number / 2)
+    result= halve(100)
+    print(result)
+
